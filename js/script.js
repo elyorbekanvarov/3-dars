86875c4bf745d685733ce46243b3298532191fae
elyorbekanvarov <e0275king@gmail.com>
2024-07-19 08:30:45 +0500

    3-dars

---
+++ js/script.js
@@ -0,0 +1,224 @@
+// If ga oid masalalar
+
+// 1 - masala
+
+// let yoshKirit = + prompt("Yoshingizni kiriting");
+// let res;
+// if (yoshKirit == 18) {
+//     res = "Siz katta yoshdagisiz"
+// } else {
+//     res = "Siz hali yoshsiz"
+// }
+// console.log(res);
+
+// 2 - masala
+
+// let sonKirit = + prompt(`Son kiriting`);
+// let res;
+// if (sonKirit % 2 == 0) {
+//     res = 'Juft son'
+// }
+// if (sonKirit % 2 == 1) {
+//     res = 'Toq son'
+// }
+// console.log(res);
+
+// 3 - masala
+
+// let num = + prompt('Son kiriting');
+// let res;
+// if (num > 0) {
+//     res = "Musbat"
+// }
+// if (num < 0) {
+//     res = 'Manfiy'
+// }
+// console.log(res);
+
+// 4 - masala
+
+// let haroratAniqla = + prompt("Haroratni raqamlarda kiriting");
+// let res;
+// if (haroratAniqla > 20) {
+//     res = "Yengil kiying"
+// } else {
+//     res = "Qalin kiying"
+// }
+// console.log(res);
+
+// 7 - masala
+
+// let narx = + prompt("Narxni summada kiriting");
+// let res;
+// if (narx > 100) {
+//     res = 'Chegirma qollanildi'
+// } else {
+//     res = "Chegirma mavjud emas"
+// }
+// console.log(res);
+
+// Ternary ga oid masalalar
+
+// 1 - masala
+
+// let harorat = + prompt("Haroratni raqamlarda kiriting");
+// let res;
+// harorat > 25 ? res = 'Issiq' : res = 'Sovuq';
+// console.log(res);
+
+// 2 - masala
+
+// let ballKirit = + prompt("Ballni kiriting");
+// let res;
+// ballKirit > 50 ? res = 'Otdi' : res = 'Yiqildi';
+// console.log(res);
+
+// 3 - masala
+
+// let sonAniqla = + prompt("Sonni kiriting");
+// let res;
+// sonAniqla > 0 ? res = 'Musbat' : res ;'Manfiy';
+// console.log(res);
+
+// For ga oid masalalar
+
+// 1 - masala
+
+// for(let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// 2 - masala
+
+// for(let i = 10; i >= 1; i--) {
+//     console.log(i);
+// }
+
+// 3 - masala
+
+// for(let i = 1; i <= 100; i += 2) {
+//     console.log(i);
+// }
+
+// 4 - masala
+
+// for(let i = 1; i <= 100; i++) {
+//     if (i % 2 == 1) {
+//         console.log(i);
+//     }
+// }
+
+// 5 - masala
+
+// let sum = 0;
+// for(let i = 1; i <= 10; i++) {
+//   sum = sum + i;
+// }
+// console.log(sum);
+
+// 6 - masala
+
+// let p = 1;
+// for(let i = 1; i <= 10; i++) {
+//     p = p * i;
+// }
+// console.log(p);
+
+// 7 - masala
+
+// let son = + prompt('Son kiriting');
+// let faktorial = 1;
+// for(let i = 1; i <= son; i++) {
+//     faktorial *= 1;
+// }
+// console.log(faktorial);
+
+// 8 - masala
+
+// for(let i = 1; i <= 50; i++) {
+//     let kvadrat = i * i;
+//     console.log(kvadrat);
+
+// 9 - masala
+
+// for(let i = 1; i <= 20; i++) {
+//     let kub = i * i * i;
+//     console.log(kub);
+// }
+
+// 10 - masala
+
+// let sum = 0;
+// for(let i = 1; i <= 100; i++) {
+//     sum += i;
+//     console.log(sum);
+// }
+
+// 11 - masala
+
+// let str = ''
+// for(let i = 1; i <= 5; i++) {
+//     str += 'Salom';
+//     console.log(str);
+// }
+
+// 14 - masala
+
+// let sonKirit = + prompt("Son kiriting");
+// for(let i = 1; i <= sonKirit; i++) {
+//     let kvadrat = i * i;
+//     console.log(kvadrat);
+// }
+
+// Boolean ga oid masalalar
+
+// 1 - masala
+
+// let A = + prompt("A sonini kiriting");
+// let B = + prompt("B sonini kiriting");
+// let res;
+// if (A % 2 == 1 && B % 2 == 1 || A % 2 == 0 && B % 2 == 0) {
+//     res = 'Rost';
+// } else {
+//     res = 'Yolgon';
+// }
+// console.log(res);
+
+// 2 - masala
+
+// let A = + prompt("A sonini kiriting");
+// let B = + prompt('B sonini kiriting');
+// let C = + prompt(`C sonini kiriting`);
+// let res;
+// if (A > 0 && B > 0 && C > 0) {
+//     res = 'Rost';
+// } else {
+//     res = 'Yolgon'
+// }
+// console.log(res);
+
+// 3 - masala
+
+// let A = + prompt("A sonini kiriting");
+// let B = + prompt('B sonini kiriting');
+// let C = + prompt(`C sonini kiriting`);
+// let res;
+// if (A > 0 || B > 0 || C > 0) {
+//     res = 'Rost';
+// } else {
+//     res = 'Yolgon'
+// }
+// console.log(res);
+
+// 5 - masala
+
+// let A = + prompt("A sonini kiriting");
+// let B = + prompt('B sonini kiriting');
+// let C = + prompt(`C sonini kiriting`);
+// let res;
+// if (A > 0 || B > 0 && C > 0) {
+//     res = 'Rost';
+// } else {
+//     res = 'Yolgon'
+// }
+// console.log(res);
